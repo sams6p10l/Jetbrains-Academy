e168ca1be2d224f51ce91575c68e6c42a970ec98
sams6p10l <63674185+sams6p10l@users.noreply.github.com>
2024-12-23 10:05:18 +0100

    File Manager: Stage 2/5:What do we have here?

--- File Manager/File Manager/task/manager.py
+++ File Manager/File Manager/task/manager.py
@@ -1,14 +1,38 @@
 import os
+
 os.chdir('module/root_folder')
 
-
-cmd = input('Input the command: ')
+cmd = input()
 cmd = cmd.strip()
 
 while cmd != 'quit':
     try:
         if cmd == 'pwd':
             print(os.getcwd())
+
+        elif cmd == 'ls':
+            entries = os.listdir()
+            directories = sorted([entry for entry in entries if os.path.isdir(entry)])
+            files = sorted([entry for entry in entries if os.path.isfile(entry)])
+
+            print(f'{len(directories)} directories and {len(files)} files')
+            print('\n'.join(directories + files))
+
+        elif cmd == 'ls -l':
+            for file in os.listdir():
+                print(f'{file} {os.stat(file).st_size}')
+
+        elif cmd == 'ls -lh':
+            for file in os.listdir():
+                size = os.stat(file).st_size
+                if size < 1024:
+                    print(f'{file} {size}B')
+                elif size < 1024 * 1024:
+                    print(f'{file} {size // 1024}KB')
+                elif size < 1024 * 1024 * 1024:
+                    print(f'{file} {size // 1024 // 1024}MB')
+                elif size < 1024 * 1024 * 1024 * 1024:
+                    print(f'{file} {size // 1024 // 1024 // 1024}GB')
 
         elif cmd == 'cd ..':
             os.chdir('..')
